8681a7c4adfe2425716e47a21624aa7c901853b2
Rohan <rohansalunkhe700@gmail.com>
2024-12-21 18:51:27 +0530

    imp files

---
+++ revite.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const cssFiles = [
+    path.join(process.cwd(),'src','App.css'),
+    path.join(process.cwd(),'src','index.css'),
+];
+
+cssFiles.forEach(file => {
+    fs.writeFileSync(file,'', 'utf8');
+});
+
+const appjsx = path.join(process.cwd(),'src','App.jsx');
+
+const content = 
+`
+import React from React;
+
+function App(){
+    return(
+        <>
+        <h1>Code like its your last commit!</h1>
+        </>
+    );
+
+}
+export default App;
+`;
+
+fs.writeFileSync(appjsx,content.trim(),'utf8');
+console.log(`ready to code!`);
